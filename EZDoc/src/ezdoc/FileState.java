2dfa4e51544c351db4148cb45a6e6511c642bfc1
tonylulciuc <tuf77259@temple.edu>
2016-04-13 09:52:17 -0400

    +Created to provide meaningful information about file state

---
+++ EZDoc/src/ezdoc/FileState.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ezdoc;
+
+/**
+ * Provides information about file state
+ * @author Antony
+ */
+public enum FileState {
+    FILE_INACTIVE,
+    FILE_ACTIVE,
+    FILE_SUCCESS,
+    FILE_FAIL
+}
