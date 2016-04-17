13c1a56f838e92c4431640dcde5920a820ffb272
pcoulomb <tue49989@temple.edu>
2016-04-17 01:38:32 -0400

    Updated methodDec in ScriptMethods class to ensure only one opening bracket per Method.

--- EZDoc/src/ezdoc/ScriptMethods.java
+++ EZDoc/src/ezdoc/ScriptMethods.java
@@ -23,40 +23,35 @@ public class ScriptMethods {
     public static String tableHeaderTagEnd;
     public static String openingBracket;
     public static String closingBracket;
-    private static boolean firstMethod=false;
-    
+    private static boolean firstMethod = false;
+
     public static String classDec(String s) {
-        
+
         StringBuilder returnHTML = new StringBuilder();
         String temp1;
         int i = 0;
         int iNext = 0;
-        
+
         /*If the string passed is null, simply return the empty string*/
-        if(s == null || s.equals("")){
+        if (s == null || s.equals("")) {
             return "";
         }
-        
-        
+
         // ADDED : Sentinel value for while loop
         //         Loops forever or breaks if '{' not found
         int iSize = s.length();
-        
+
         while (i < iSize && s.charAt(i) != '{') {
             i++;
         }
 
         temp1 = s.substring(0, i);
 
-        
-
         returnHTML.append("<div class='Class'><br>");
-        
-        
+
         i = 0;
         while (i < temp1.length()) {
-            switch (temp1.charAt(i))
-            {
+            switch (temp1.charAt(i)) {
                 case '<':
                     returnHTML.append(lessThan);
                     break;
@@ -65,18 +60,18 @@ public class ScriptMethods {
                     break;
                 default:
                     // Check for extra documentation
-                    if ((iNext = fileHeaderDocumentation(returnHTML, temp1, i)) == i)
-                    {
+                    if ((iNext = fileHeaderDocumentation(returnHTML, temp1, i)) == i) {
                         // Check for packages/imports
-                         if ((iNext = fileDependencies(returnHTML, temp1, i)) == i)  
-                              returnHTML.append(temp1.charAt(i));
-                         else
-                             i = iNext - 1;
-                    }
-                    else
+                        if ((iNext = fileDependencies(returnHTML, temp1, i)) == i) {
+                            returnHTML.append(temp1.charAt(i));
+                        } else {
+                            i = iNext - 1;
+                        }
+                    } else {
                         i = iNext - 1;
+                    }
             }
-            
+
             i++;
         }
         returnHTML.append("</div><br>");
@@ -89,16 +84,17 @@ public class ScriptMethods {
         String temp1;
         String returnStatement;
         StringBuilder returnHTML = new StringBuilder();
-        
+        boolean openBracket = false;
+
         /*If the string passed is null, simply return the empty string*/
-        if(s == null || s.equals("")){
+        if (s == null || s.equals("")) {
             return "";
         }
-        
+
         s.trim();
 
         int i = 2;
-        while (s.charAt(i) != '\n' && i<s.length()-1) {
+        while (s.charAt(i) != '\n' && i < s.length() - 1) {
             i++;
         }
         i++;
@@ -108,32 +104,38 @@ public class ScriptMethods {
         returnHTML.append(outerFormatTagEnd);
         returnHTML.append(outerFormatTagStart);
         returnHTML.append("<br><div class='method'>");
-        
-        if(firstMethod){
-        returnHTML.append(closingBracket);
+
+        if (firstMethod) {
+            returnHTML.append(closingBracket);
         }
-        
+
         returnHTML.append("<br>");
-        
+
         i = 0;
         returnHTML.append(tableHeaderTagStart);
-        
+
         while (i < temp1.length()) {
 
             if (temp1.charAt(i) == '<') {
                 returnHTML.append(lessThan);
             } else if (temp1.charAt(i) == '>') {
                 returnHTML.append(greaterThan);
+            } else if (temp1.charAt(i) == '{') {
+                returnHTML.append(temp1.charAt(i));
+                openBracket = true;
             } else {
                 returnHTML.append(temp1.charAt(i));
+
             }
             i++;
         }
         returnHTML.append(tableHeaderTagEnd);
-        returnHTML.append(openingBracket);
+        if (!openBracket) {
+            returnHTML.append(openingBracket);
+        }
         returnHTML.append("</div><br>");
-        
-        firstMethod=true;
+
+        firstMethod = true;
 
         return returnHTML.toString();
     }
@@ -143,12 +145,12 @@ public class ScriptMethods {
         String temp2;
         String returnStatement;
         StringBuilder returnHTML = new StringBuilder();
-        
-         /*If the string passed is null, simply return the empty string*/
-        if(s == null || s.equals("")){
+
+        /*If the string passed is null, simply return the empty string*/
+        if (s == null || s.equals("")) {
             return "";
         }
-        
+
         temp1 = s.replaceAll("/", "");
         temp2 = temp1.replaceAll("\\*", "");
         temp2.trim();
@@ -160,13 +162,12 @@ public class ScriptMethods {
 
         returnStatement = temp2.substring(i, temp2.length());
 
-        
         returnHTML.append("<div class='param'>");
         returnHTML.append(innerFormatTagStart);
         returnHTML.append(tableDataTagStart);
         returnHTML.append("Parameter:<br>");
         returnHTML.append(tableDataTagEnd);
-        
+
         returnHTML.append(tableDataTagStart);
         i = 0;
         while (i < returnStatement.length()) {
@@ -183,7 +184,7 @@ public class ScriptMethods {
         returnHTML.append(tableDataTagEnd);
         returnHTML.append(innerFormatTagEnd);
         returnHTML.append("</div>");
-        
+
         returnHTML.append("<br>");
 
         return returnHTML.toString();
@@ -198,12 +199,12 @@ public class ScriptMethods {
         String temp2;
         String returnStatement;
         StringBuilder returnHTML = new StringBuilder();
-        
-         /*If the string passed is null, simply return the empty string*/
-        if(s == null || s.equals("")){
+
+        /*If the string passed is null, simply return the empty string*/
+        if (s == null || s.equals("")) {
             return "";
         }
-        
+
         temp1 = s.replaceAll("/", "");
         temp2 = temp1.replaceAll("\\*", "");
         temp2.trim();
@@ -215,7 +216,6 @@ public class ScriptMethods {
 
         returnStatement = temp2.substring(i, temp2.length());
 
-        
         returnHTML.append("<div class='return'>");
         returnHTML.append(innerFormatTagStart);
         returnHTML.append(tableDataTagStart);
@@ -223,7 +223,7 @@ public class ScriptMethods {
         returnHTML.append(tableDataTagEnd);
         i = 0;
         returnHTML.append(tableDataTagStart);
-        
+
         while (i < returnStatement.length()) {
 
             if (returnStatement.charAt(i) == '<') {
@@ -235,11 +235,11 @@ public class ScriptMethods {
             }
             i++;
         }
-        
+
         returnHTML.append(tableDataTagEnd);
         returnHTML.append(innerFormatTagEnd);
         returnHTML.append("</div>");
-        
+
         returnHTML.append("<br>");
 
         return returnHTML.toString();
@@ -250,25 +250,24 @@ public class ScriptMethods {
         String temp2;
 
         StringBuilder returnHTML = new StringBuilder();
-        
-          /*If the string passed is null, simply return the empty string*/
-        if(s == null || s.equals("")){
+
+        /*If the string passed is null, simply return the empty string*/
+        if (s == null || s.equals("")) {
             return "";
         }
-        
+
         temp1 = s.replace("/", "");
         temp2 = temp1.replaceAll("\\*", "");
         temp2.trim();
 
-        
         returnHTML.append("<div class='description'>");
         returnHTML.append(innerFormatTagStart);
         returnHTML.append(tableDataTagStart);
         returnHTML.append("Description:<br>");
         returnHTML.append(tableDataTagEnd);
-        
+
         returnHTML.append(tableDataTagStart);
-        
+
         int i = 0;
         while (i < temp2.length()) {
 
@@ -284,38 +283,38 @@ public class ScriptMethods {
         returnHTML.append(tableDataTagEnd);
         returnHTML.append(innerFormatTagEnd);
         returnHTML.append("</div>");
-        
+
         returnHTML.append("<br>");
 
         return returnHTML.toString();
 
     }
-    
+
     /**
-     * Attempts to locate extra information provided by user in begining of 
+     * Attempts to locate extra information provided by user in begining of
      * document.
+     *
      * @param _bldHtml[in] HTML data
      * @param _raw[in] Raw file data
      * @param _index [in] current position in raw data
      * @return integer[out] next position in _raw
      */
-    private static int fileHeaderDocumentation(StringBuilder _bldHtml, String _raw, int _index)
-    {
-        
+    private static int fileHeaderDocumentation(StringBuilder _bldHtml, String _raw, int _index) {
+
         return (_index);
     }
-    
+
     /**
-     * Attempts to locate extra information provided by user in begining of 
+     * Attempts to locate extra information provided by user in begining of
      * document.
+     *
      * @param _bldHtml[in] HTML data
      * @param _raw[in] Raw file data
      * @param _index [in] current position in raw data
      * @return integer[out] next position in _raw
      */
-    private static int fileDependencies(StringBuilder _bldHtml, String _raw, int _index)
-    {
-        
+    private static int fileDependencies(StringBuilder _bldHtml, String _raw, int _index) {
+
         return (_index);
     }
 }
